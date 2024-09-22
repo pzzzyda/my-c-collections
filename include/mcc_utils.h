0663dd63462f3ffe1565a5519df134863dfea1c5
pzzzyda <zhongyuan03@outlook.com>
2024-09-22 17:00:38 +0800

    Modified mcc_memswap function.

--- include/mcc_utils.h
+++ include/mcc_utils.h
@@ -1,31 +1,27 @@
 #ifndef _MCC_UTILS_H
 #define _MCC_UTILS_H
 
-#include "mcc_type.h"
-#include <stdlib.h>
+#include "core/mcc_types.h"
 #include <string.h>
 
 static inline void mcc_memswap(void *p1, void *p2, size_t n)
 {
-	uint8_t *tmp;
-	if (n >= 32 && ((tmp = malloc(n)) != NULL)) {
-		memcpy(tmp, p1, n);
-		memcpy(p1, p2, n);
-		memcpy(p2, tmp, n);
-		free(tmp);
-	} else {
-		uint8_t t[32];
-		while (n >= 32) {
-			memcpy(t, p1, 32);
-			p1 = (uint8_t *)memcpy(p1, p2, 32) + 32;
-			p2 = (uint8_t *)memcpy(p2, t, 32) + 32;
-			n -= 32;
-		}
-		while (n > 0) {
-			t[0] = ((uint8_t *)p1)[--n];
-			((uint8_t *)p1)[n] = ((uint8_t *)p2)[n];
-			((uint8_t *)p2)[n] = t[0];
-		}
+	mcc_u8 tmp[32];
+	mcc_u8 *b1 = p1;
+	mcc_u8 *b2 = p2;
+	while (n >= 32) {
+		memcpy(tmp, b1, 32);
+		memcpy(b1, b2, 32);
+		memcpy(b2, tmp, 32);
+		b1 += 32;
+		b2 += 32;
+		n -= 32;
+	}
+	while (n > 0) {
+		tmp[0] = *b1;
+		*b1++ = *b2;
+		*b2++ = tmp[0];
+		n -= 1;
 	}
 }
 
