57a650b347ced63de839d7dda3850eb54ac9faf0
pzzzyda <zhongyuan03@outlook.com>
2024-09-22 16:59:04 +0800

    Defined the mcc_err.

--- include/mcc_err.h
+++ include/mcc_err.h
@@ -1,12 +1,12 @@
 #ifndef _MCC_ERR_H
 #define _MCC_ERR_H
 
-enum {
+typedef enum {
 	OK,
 	NONE,
 	INVALID_ARGUMENTS,
 	CANNOT_ALLOCATE_MEMORY,
 	OUT_OF_RANGE,
-};
+} mcc_err;
 
 #endif /* _MCC_ERR_H */
