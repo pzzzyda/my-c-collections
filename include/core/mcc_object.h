ced58f6e94d2492161e1f42fd7b1e5e450aae4a9
pzzzyda <zhongyuan03@outlook.com>
2024-10-14 16:28:38 +0800

    Refactored mcc_object.

--- include/core/mcc_object.h
+++ include/core/mcc_object.h
@@ -5,121 +5,35 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef void (*mcc_destruct_fn)(void *self);
+typedef void (*mcc_drop_fn)(void *self);
 typedef int (*mcc_compare_fn)(const void *self, const void *other);
 typedef size_t (*mcc_hash_fn)(const void *key);
 
 struct mcc_object_interface {
 	const size_t size;
-	const mcc_destruct_fn dtor;
+	const mcc_drop_fn drop;
 	const mcc_compare_fn cmp;
 	const mcc_hash_fn hash;
 };
 
 typedef const char *mcc_str_t;
 
-#ifndef CHAR
-#define CHAR (&__mcc_char_obj_intf)
-#endif
-#ifndef UCHAR
-#define UCHAR (&__mcc_uchar_obj_intf)
-#endif
-#ifndef SHORT
-#define SHORT (&__mcc_short_obj_intf)
-#endif
-#ifndef USHORT
-#define USHORT (&__mcc_ushort_obj_intf)
-#endif
-#ifndef INT
-#define INT (&__mcc_int_obj_intf)
-#endif
-#ifndef UINT
-#define UINT (&__mcc_uint_obj_intf)
-#endif
-#ifndef LONG
-#define LONG (&__mcc_long_obj_intf)
-#endif
-#ifndef ULONG
-#define ULONG (&__mcc_ulong_obj_intf)
-#endif
-#ifndef LONG_LONG
-#define LONG_LONG (&__mcc_long_long_obj_intf)
-#endif
-#ifndef ULONG_LONG
-#define ULONG_LONG (&__mcc_ulong_long_obj_intf)
-#endif
-#ifndef FLOAT
-#define FLOAT (&__mcc_float_obj_intf)
-#endif
-#ifndef DOUBLE
-#define DOUBLE (&__mcc_double_obj_intf)
-#endif
-#ifndef LONG_DOUBLE
-#define LONG_DOUBLE (&__mcc_long_double_obj_intf)
-#endif
-#ifndef INT8_T
-#define INT8_T (&__mcc_int8_t_obj_intf)
-#endif
-#ifndef UINT8_T
-#define UINT8_T (&__mcc_uint8_t_obj_intf)
-#endif
-#ifndef INT16_T
-#define INT16_T (&__mcc_int16_t_obj_intf)
-#endif
-#ifndef UINT16_T
-#define UINT16_T (&__mcc_uint16_t_obj_intf)
-#endif
-#ifndef INT32_T
-#define INT32_T (&__mcc_int32_t_obj_intf)
-#endif
-#ifndef UINT32_T
-#define UINT32_T (&__mcc_uint32_t_obj_intf)
-#endif
-#ifndef INT64_T
-#define INT64_T (&__mcc_int64_t_obj_intf)
-#endif
-#ifndef UINT64_T
-#define UINT64_T (&__mcc_uint64_t_obj_intf)
-#endif
-#ifndef SIZE_T
-#define SIZE_T (&__mcc_size_t_obj_intf)
-#endif
-#ifndef STR
-#define STR (&__mcc_str_obj_intf)
-#endif
-#define MCC_VECTOR (&__mcc_vector_obj_intf)
-#define MCC_DEQUE (&__mcc_deque_obj_intf)
-#define MCC_LIST (&__mcc_list_obj_intf)
-#define MCC_HASH_MAP (&__mcc_hash_map_obj_intf)
-#define MCC_RB_MAP (&__mcc_rb_map_obj_intf)
+const struct mcc_object_interface *mcc_str(void);
 
-extern const struct mcc_object_interface __mcc_char_obj_intf;
-extern const struct mcc_object_interface __mcc_uchar_obj_intf;
-extern const struct mcc_object_interface __mcc_short_obj_intf;
-extern const struct mcc_object_interface __mcc_ushort_obj_intf;
-extern const struct mcc_object_interface __mcc_int_obj_intf;
-extern const struct mcc_object_interface __mcc_uint_obj_intf;
-extern const struct mcc_object_interface __mcc_long_obj_intf;
-extern const struct mcc_object_interface __mcc_ulong_obj_intf;
-extern const struct mcc_object_interface __mcc_long_long_obj_intf;
-extern const struct mcc_object_interface __mcc_ulong_long_obj_intf;
-extern const struct mcc_object_interface __mcc_float_obj_intf;
-extern const struct mcc_object_interface __mcc_double_obj_intf;
-extern const struct mcc_object_interface __mcc_long_double_obj_intf;
-extern const struct mcc_object_interface __mcc_int8_t_obj_intf;
-extern const struct mcc_object_interface __mcc_uint8_t_obj_intf;
-extern const struct mcc_object_interface __mcc_int16_t_obj_intf;
-extern const struct mcc_object_interface __mcc_uint16_t_obj_intf;
-extern const struct mcc_object_interface __mcc_int32_t_obj_intf;
-extern const struct mcc_object_interface __mcc_uint32_t_obj_intf;
-extern const struct mcc_object_interface __mcc_int64_t_obj_intf;
-extern const struct mcc_object_interface __mcc_uint64_t_obj_intf;
-extern const struct mcc_object_interface __mcc_size_t_obj_intf;
-extern const struct mcc_object_interface __mcc_str_obj_intf;
-extern const struct mcc_object_interface __mcc_vector_obj_intf;
-extern const struct mcc_object_interface __mcc_deque_obj_intf;
-extern const struct mcc_object_interface __mcc_list_obj_intf;
-extern const struct mcc_object_interface __mcc_hash_map_obj_intf;
-extern const struct mcc_object_interface __mcc_rb_map_obj_intf;
+const struct mcc_object_interface *mcc_char(void);
+const struct mcc_object_interface *mcc_short(void);
+const struct mcc_object_interface *mcc_int(void);
+const struct mcc_object_interface *mcc_long(void);
+const struct mcc_object_interface *mcc_long_long(void);
+
+const struct mcc_object_interface *mcc_uchar(void);
+const struct mcc_object_interface *mcc_ushort(void);
+const struct mcc_object_interface *mcc_uint(void);
+const struct mcc_object_interface *mcc_ulong(void);
+const struct mcc_object_interface *mcc_ulong_long(void);
+
+const struct mcc_object_interface *mcc_float(void);
+const struct mcc_object_interface *mcc_double(void);
+const struct mcc_object_interface *mcc_long_double(void);
 
 #endif /* _MCC_OBJECT_H */
--- src/mcc_object.c
+++ src/mcc_object.c
@@ -1,470 +1,347 @@
 #include "core/mcc_object.h"
 #include <string.h>
 
-static int mcc_char_cmp(const void *self, const void *other)
-{
-	const signed char *a = self;
-	const signed char *b = other;
+/* mcc_str_t (const char *) */
 
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
+static int mcc_str_cmp(const mcc_str_t *self, const mcc_str_t *other)
+{
+	return strcmp(*self, *other);
 }
 
-static size_t mcc_char_hash(const void *key)
+static size_t mcc_str_hash(const mcc_str_t *key)
 {
-	return *(signed char *)key;
-}
-
-static int mcc_uchar_cmp(const void *self, const void *other)
-{
-	const unsigned char *a = self;
-	const unsigned char *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_uchar_hash(const void *key)
-{
-	return *(unsigned char *)key;
-}
-
-static int mcc_short_cmp(const void *self, const void *other)
-{
-	const signed short *a = self;
-	const signed short *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_short_hash(const void *key)
-{
-	return *(signed short *)key;
-}
-
-static int mcc_ushort_cmp(const void *self, const void *other)
-{
-	const unsigned short *a = self;
-	const unsigned short *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_ushort_hash(const void *key)
-{
-	return *(unsigned short *)key;
-}
-
-static int mcc_int_cmp(const void *self, const void *other)
-{
-	const signed int *a = self;
-	const signed int *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_int_hash(const void *key)
-{
-	return *(signed int *)key;
-}
-
-static int mcc_uint_cmp(const void *self, const void *other)
-{
-	const unsigned int *a = self;
-	const unsigned int *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_uint_hash(const void *key)
-{
-	return *(unsigned int *)key;
-}
-
-static int mcc_long_cmp(const void *self, const void *other)
-{
-	const signed long *a = self;
-	const signed long *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_long_hash(const void *key)
-{
-	return *(signed long *)key;
-}
-
-static int mcc_ulong_cmp(const void *self, const void *other)
-{
-	const unsigned long *a = self;
-	const unsigned long *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_ulong_hash(const void *key)
-{
-	return *(unsigned long *)key;
-}
-
-static int mcc_long_long_cmp(const void *self, const void *other)
-{
-	const signed long long *a = self;
-	const signed long long *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_long_long_hash(const void *key)
-{
-	return *(signed long long *)key;
-}
-
-static int mcc_ulong_long_cmp(const void *self, const void *other)
-{
-	const unsigned long long *a = self;
-	const unsigned long long *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_ulong_long_hash(const void *key)
-{
-	return *(unsigned long long *)key;
-}
-
-static int mcc_int8_t_cmp(const void *self, const void *other)
-{
-	const int8_t *a = self;
-	const int8_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_int8_t_hash(const void *key)
-{
-	return *(int8_t *)key;
-}
-
-static int mcc_uint8_t_cmp(const void *self, const void *other)
-{
-	const uint8_t *a = self;
-	const uint8_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_uint8_t_hash(const void *key)
-{
-	return *(uint8_t *)key;
-}
-
-static int mcc_int16_t_cmp(const void *self, const void *other)
-{
-	const int16_t *a = self;
-	const int16_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_int16_t_hash(const void *key)
-{
-	return *(int16_t *)key;
-}
-
-static int mcc_uint16_t_cmp(const void *self, const void *other)
-{
-	const uint16_t *a = self;
-	const uint16_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_uint16_t_hash(const void *key)
-{
-	return *(uint16_t *)key;
-}
-
-static int mcc_int32_t_cmp(const void *self, const void *other)
-{
-	const int32_t *a = self;
-	const int32_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_int32_t_hash(const void *key)
-{
-	return *(int32_t *)key;
-}
-
-static int mcc_uint32_t_cmp(const void *self, const void *other)
-{
-	const uint32_t *a = self;
-	const uint32_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_uint32_t_hash(const void *key)
-{
-	return *(uint32_t *)key;
-}
-
-static int mcc_int64_t_cmp(const void *self, const void *other)
-{
-	const int64_t *a = self;
-	const int64_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_int64_t_hash(const void *key)
-{
-	return *(int64_t *)key;
-}
-
-static int mcc_uint64_t_cmp(const void *self, const void *other)
-{
-	const uint64_t *a = self;
-	const uint64_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_uint64_t_hash(const void *key)
-{
-	return *(uint64_t *)key;
-}
-
-static int mcc_float_cmp(const void *self, const void *other)
-{
-	const float *a = self;
-	const float *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_float_hash(const void *key)
-{
-	return *(float *)key;
-}
-
-static int mcc_double_cmp(const void *self, const void *other)
-{
-	const double *a = self;
-	const double *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_double_hash(const void *key)
-{
-	return *(double *)key;
-}
-
-static int mcc_long_double_cmp(const void *self, const void *other)
-{
-	const long double *a = self;
-	const long double *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_long_double_hash(const void *key)
-{
-	return *(long double *)key;
-}
-
-static int mcc_size_t_cmp(const void *self, const void *other)
-{
-	const size_t *a = self;
-	const size_t *b = other;
-
-	return *a > *b ? 1 : (*a == *b ? 0 : -1);
-}
-
-static size_t mcc_size_t_hash(const void *key)
-{
-	return *(size_t *)key;
-}
-
-static int mcc_str_cmp(const void *self, const void *other)
-{
-	const mcc_str_t *a = self;
-	const mcc_str_t *b = other;
-
-	return strcmp(*a, *b);
-}
-
-static size_t mcc_str_hash(const void *key)
-{
-	const char *c = *((const mcc_str_t *)key);
+	const char *c = *key;
 	size_t h = 0;
 
-	while (*c) {
-		h = h * 31 + *c;
-		c++;
-	}
+	while (*c)
+		h = h * 31 + *c++;
 
 	return h;
 }
 
-const struct mcc_object_interface __mcc_char_obj_intf = {
-	.size = sizeof(char),
-	.dtor = NULL,
-	.cmp = &mcc_char_cmp,
-	.hash = &mcc_char_hash,
-};
-
-const struct mcc_object_interface __mcc_uchar_obj_intf = {
-	.size = sizeof(unsigned char),
-	.dtor = NULL,
-	.cmp = &mcc_uchar_cmp,
-	.hash = &mcc_uchar_hash,
-};
-
-const struct mcc_object_interface __mcc_short_obj_intf = {
-	.size = sizeof(short),
-	.dtor = NULL,
-	.cmp = &mcc_short_cmp,
-	.hash = &mcc_short_hash,
-};
-
-const struct mcc_object_interface __mcc_ushort_obj_intf = {
-	.size = sizeof(unsigned short),
-	.dtor = NULL,
-	.cmp = &mcc_ushort_cmp,
-	.hash = &mcc_ushort_hash,
-};
-
-const struct mcc_object_interface __mcc_int_obj_intf = {
-	.size = sizeof(int),
-	.dtor = NULL,
-	.cmp = &mcc_int_cmp,
-	.hash = &mcc_int_hash,
-};
-
-const struct mcc_object_interface __mcc_uint_obj_intf = {
-	.size = sizeof(unsigned int),
-	.dtor = NULL,
-	.cmp = &mcc_uint_cmp,
-	.hash = &mcc_uint_hash,
-};
-
-const struct mcc_object_interface __mcc_long_obj_intf = {
-	.size = sizeof(long),
-	.dtor = NULL,
-	.cmp = &mcc_long_cmp,
-	.hash = &mcc_long_hash,
-};
-
-const struct mcc_object_interface __mcc_ulong_obj_intf = {
-	.size = sizeof(char),
-	.dtor = NULL,
-	.cmp = &mcc_ulong_cmp,
-	.hash = &mcc_ulong_hash,
-};
-
-const struct mcc_object_interface __mcc_long_long_obj_intf = {
-	.size = sizeof(long long),
-	.dtor = NULL,
-	.cmp = &mcc_long_long_cmp,
-	.hash = &mcc_long_long_hash,
-};
-
-const struct mcc_object_interface __mcc_ulong_long_obj_intf = {
-	.size = sizeof(unsigned long long),
-	.dtor = NULL,
-	.cmp = &mcc_ulong_long_cmp,
-	.hash = &mcc_ulong_long_hash,
-};
-
-const struct mcc_object_interface __mcc_float_obj_intf = {
-	.size = sizeof(float),
-	.dtor = NULL,
-	.cmp = &mcc_float_cmp,
-	.hash = &mcc_float_hash,
-};
-
-const struct mcc_object_interface __mcc_double_obj_intf = {
-	.size = sizeof(double),
-	.dtor = NULL,
-	.cmp = &mcc_double_cmp,
-	.hash = &mcc_double_hash,
-};
-
-const struct mcc_object_interface __mcc_long_double_obj_intf = {
-	.size = sizeof(long double),
-	.dtor = NULL,
-	.cmp = &mcc_long_double_cmp,
-	.hash = &mcc_long_double_hash,
-};
-
-const struct mcc_object_interface __mcc_int8_t_obj_intf = {
-	.size = sizeof(int8_t),
-	.dtor = NULL,
-	.cmp = &mcc_int8_t_cmp,
-	.hash = &mcc_int8_t_hash,
-};
-
-const struct mcc_object_interface __mcc_int16_t_obj_intf = {
-	.size = sizeof(int16_t),
-	.dtor = NULL,
-	.cmp = &mcc_int16_t_cmp,
-	.hash = &mcc_int16_t_hash,
-};
-
-const struct mcc_object_interface __mcc_int32_t_obj_intf = {
-	.size = sizeof(int),
-	.dtor = NULL,
-	.cmp = &mcc_int32_t_cmp,
-	.hash = &mcc_int32_t_hash,
-};
-
-const struct mcc_object_interface __mcc_int64_t_obj_intf = {
-	.size = sizeof(int64_t),
-	.dtor = NULL,
-	.cmp = &mcc_int64_t_cmp,
-	.hash = &mcc_int64_t_hash,
-};
-
-const struct mcc_object_interface __mcc_uint8_t_obj_intf = {
-	.size = sizeof(uint8_t),
-	.dtor = NULL,
-	.cmp = &mcc_uint8_t_cmp,
-	.hash = &mcc_uint8_t_hash,
-};
-
-const struct mcc_object_interface __mcc_uint16_t_obj_intf = {
-	.size = sizeof(uint16_t),
-	.dtor = NULL,
-	.cmp = &mcc_uint16_t_cmp,
-	.hash = &mcc_uint16_t_hash,
-};
-
-const struct mcc_object_interface __mcc_uint32_t_obj_intf = {
-	.size = sizeof(uint32_t),
-	.dtor = NULL,
-	.cmp = &mcc_uint32_t_cmp,
-	.hash = &mcc_uint32_t_hash,
-};
-
-const struct mcc_object_interface __mcc_uint64_t_obj_intf = {
-	.size = sizeof(uint64_t),
-	.dtor = NULL,
-	.cmp = &mcc_uint64_t_cmp,
-	.hash = &mcc_uint64_t_hash,
-};
-
-const struct mcc_object_interface __mcc_size_t_obj_intf = {
-	.size = sizeof(size_t),
-	.dtor = NULL,
-	.cmp = &mcc_size_t_cmp,
-	.hash = &mcc_size_t_hash,
-};
-
-const struct mcc_object_interface __mcc_str_obj_intf = {
+static const struct mcc_object_interface mcc_str_obj_intf = {
 	.size = sizeof(mcc_str_t),
-	.dtor = NULL,
-	.cmp = &mcc_str_cmp,
-	.hash = &mcc_str_hash,
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_str_cmp,
+	.hash = (mcc_hash_fn)&mcc_str_hash,
 };
+
+const struct mcc_object_interface *mcc_str()
+{
+	return &mcc_str_obj_intf;
+}
+
+/* signed char */
+
+static int mcc_char_cmp(const signed char *self, const signed char *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_char_hash(const signed char *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_char_obj_intf = {
+	.size = sizeof(signed char),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_char_cmp,
+	.hash = (mcc_hash_fn)&mcc_char_hash,
+};
+
+const struct mcc_object_interface *mcc_char()
+{
+	return &mcc_char_obj_intf;
+}
+
+/* signed short */
+
+static int mcc_short_cmp(const signed short *self, const signed short *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_short_hash(const signed short *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_short_obj_intf = {
+	.size = sizeof(signed short),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_short_cmp,
+	.hash = (mcc_hash_fn)&mcc_short_hash,
+};
+
+const struct mcc_object_interface *mcc_short()
+{
+	return &mcc_short_obj_intf;
+}
+
+/* signed int */
+
+static int mcc_int_cmp(const signed int *self, const signed int *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_int_hash(const signed int *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_int_obj_intf = {
+	.size = sizeof(signed int),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_int_cmp,
+	.hash = (mcc_hash_fn)&mcc_int_hash,
+};
+
+const struct mcc_object_interface *mcc_int()
+{
+	return &mcc_int_obj_intf;
+}
+
+/* signed long */
+
+static int mcc_long_cmp(const signed long *self, const signed long *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_long_hash(const signed long *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_long_obj_intf = {
+	.size = sizeof(signed long),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_long_cmp,
+	.hash = (mcc_hash_fn)&mcc_long_hash,
+};
+
+const struct mcc_object_interface *mcc_long()
+{
+	return &mcc_long_obj_intf;
+}
+
+/* signed long long */
+
+static int mcc_long_long_cmp(const signed long long *self,
+			     const signed long long *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_long_long_hash(const signed long long *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_long_long_obj_intf = {
+	.size = sizeof(signed long long),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_long_long_cmp,
+	.hash = (mcc_hash_fn)&mcc_long_long_hash,
+};
+
+const struct mcc_object_interface *mcc_long_long()
+{
+	return &mcc_long_long_obj_intf;
+}
+
+/* unsigned char */
+
+static int mcc_uchar_cmp(const unsigned char *self, const unsigned char *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_uchar_hash(const unsigned char *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_uchar_obj_intf = {
+	.size = sizeof(unsigned char),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_uchar_cmp,
+	.hash = (mcc_hash_fn)&mcc_uchar_hash,
+};
+
+const struct mcc_object_interface *mcc_uchar()
+{
+	return &mcc_uchar_obj_intf;
+}
+
+/* unsigned short */
+
+static int mcc_ushort_cmp(const unsigned short *self,
+			  const unsigned short *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_ushort_hash(const unsigned short *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_ushort_obj_intf = {
+	.size = sizeof(unsigned short),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_ushort_cmp,
+	.hash = (mcc_hash_fn)&mcc_ushort_hash,
+};
+
+const struct mcc_object_interface *mcc_ushort()
+{
+	return &mcc_ushort_obj_intf;
+}
+
+/* unsigned int */
+
+static int mcc_uint_cmp(const unsigned int *self, const unsigned int *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_uint_hash(const unsigned int *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_uint_obj_intf = {
+	.size = sizeof(unsigned int),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_uint_cmp,
+	.hash = (mcc_hash_fn)&mcc_uint_hash,
+};
+
+const struct mcc_object_interface *mcc_uint()
+{
+	return &mcc_uint_obj_intf;
+}
+
+/* unsigned long */
+
+static int mcc_ulong_cmp(const unsigned long *self, const unsigned long *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_ulong_hash(const unsigned long *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_ulong_obj_intf = {
+	.size = sizeof(unsigned long),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_ulong_cmp,
+	.hash = (mcc_hash_fn)&mcc_ulong_hash,
+};
+
+const struct mcc_object_interface *mcc_ulong()
+{
+	return &mcc_ulong_obj_intf;
+}
+
+/* unsigned long long */
+
+static int mcc_ulong_long_cmp(const unsigned long long *self,
+			      const unsigned long long *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_ulong_long_hash(const unsigned long long *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_ulong_long_obj_intf = {
+	.size = sizeof(unsigned long long),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_ulong_long_cmp,
+	.hash = (mcc_hash_fn)&mcc_ulong_long_hash,
+};
+
+const struct mcc_object_interface *mcc_ulong_long()
+{
+	return &mcc_ulong_long_obj_intf;
+}
+
+/* float */
+
+static int mcc_float_cmp(const float *self, const float *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_float_hash(const float *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_float_obj_intf = {
+	.size = sizeof(float),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_float_cmp,
+	.hash = (mcc_hash_fn)&mcc_float_hash,
+};
+
+const struct mcc_object_interface *mcc_float()
+{
+	return &mcc_float_obj_intf;
+}
+
+/* double */
+static int mcc_double_cmp(const double *self, const double *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_double_hash(const double *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_double_obj_intf = {
+	.size = sizeof(double),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_double_cmp,
+	.hash = (mcc_hash_fn)&mcc_double_hash,
+};
+
+const struct mcc_object_interface *mcc_double()
+{
+	return &mcc_double_obj_intf;
+}
+
+/* long double */
+
+static int mcc_long_double_cmp(const long double *self,
+			       const long double *other)
+{
+	return *self > *other ? 1 : *self == *other ? 0 : -1;
+}
+
+static size_t mcc_long_double_hash(const long double *key)
+{
+	return *key;
+}
+
+static const struct mcc_object_interface mcc_long_double_obj_intf = {
+	.size = sizeof(long double),
+	.drop = (mcc_drop_fn)0,
+	.cmp = (mcc_compare_fn)&mcc_long_double_cmp,
+	.hash = (mcc_hash_fn)&mcc_long_double_hash,
+};
+
+const struct mcc_object_interface *mcc_long_double()
+{
+	return &mcc_long_double_obj_intf;
+}
