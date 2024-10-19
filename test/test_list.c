f2e8fda258d08230009289e7869432ae105786a1
pzzzyda <zhongyuan03@outlook.com>
2024-10-19 22:17:16 +0800

    Retested mcc_list.

--- test/test_list.c
+++ test/test_list.c
@@ -1,217 +1,154 @@
+#include "fruit.h"
 #include "mcc_list.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <assert.h>
 
-void print_int_list(struct mcc_list *list, const char *fmt, ...)
+static bool equals(struct mcc_list *d, int *a)
 {
-	struct mcc_list_iter iter;
-	int elem;
-	va_list args;
+	struct mcc_list_iter *iter;
+	int *ref;
 
-	va_start(args, fmt);
-	vprintf(fmt, args);
-	va_end(args);
-
-	mcc_list_iter_init(list, &iter);
-	while (mcc_iter_next(&iter, &elem)) {
-		printf("%d ", elem);
-	}
-	putchar('\n');
-}
-
-int test_push_and_pop()
-{
-	printf("%s\n", __FUNCTION__);
-	struct mcc_list *l = mcc_list_new(INT);
-
-	mcc_list_push_back(l, &(int){0});
-	mcc_list_push_back(l, &(int){1});
-	mcc_list_push_back(l, &(int){2});
-	mcc_list_push_back(l, &(int){3});
-	mcc_list_push_front(l, &(int){0});
-	mcc_list_push_front(l, &(int){1});
-	mcc_list_push_front(l, &(int){2});
-	mcc_list_push_front(l, &(int){3});
-	print_int_list(l, "Raw data:\n");
-
-	mcc_list_push_back(l, &(int){10});
-	print_int_list(l, "Push 10 in back:\n");
-
-	mcc_list_pop_front(l);
-	print_int_list(l, "Pop front:\n");
-
-	mcc_list_push_back(l, &(int){10});
-	print_int_list(l, "Push 10 in back:\n");
-
-	mcc_list_pop_front(l);
-	print_int_list(l, "Pop front:\n");
-
-	mcc_list_push_front(l, &(int){10});
-	print_int_list(l, "Push 10 in front:\n");
-
-	mcc_list_pop_back(l);
-	print_int_list(l, "Pop back:\n");
-
-	mcc_list_delete(l);
-	puts("testing done");
-	return 0;
-}
-
-int test_insert_and_remove()
-{
-	printf("%s\n", __FUNCTION__);
-	struct mcc_list *l = mcc_list_new(INT);
-
-	mcc_list_push_back(l, &(int){0});
-	mcc_list_push_back(l, &(int){1});
-	mcc_list_push_back(l, &(int){2});
-	mcc_list_push_back(l, &(int){3});
-	mcc_list_push_front(l, &(int){0});
-	mcc_list_push_front(l, &(int){1});
-	mcc_list_push_front(l, &(int){2});
-	mcc_list_push_front(l, &(int){3});
-	print_int_list(l, "Raw data:\n");
-
-	mcc_list_insert(l, 7, &(int){100});
-	print_int_list(l, "Insert 100 at 7:\n");
-
-	mcc_list_insert(l, 0, &(int){100});
-	print_int_list(l, "Insert 100 at 0:\n");
-
-	mcc_list_remove(l, 5);
-	print_int_list(l, "Remove element at 5:\n");
-
-	mcc_list_insert(l, 2, &(int){100});
-	print_int_list(l, "Insert 100 at 2:\n");
-
-	mcc_list_remove(l, 1);
-	print_int_list(l, "Remove element at 1:\n");
-
-	mcc_list_insert(l, mcc_list_len(l), &(int){100});
-	print_int_list(l, "Insert 100 at the end:\n");
-
-	mcc_list_insert(l, 1, &(int){100});
-	print_int_list(l, "Insert 100 at 1:\n");
-
-	mcc_list_delete(l);
-	puts("testing done");
-	return 0;
-}
-
-struct fruit {
-	char *name;
-	int color;
-};
-
-void fruit_dtor(void *self)
-{
-	struct fruit *fruit = self;
-
-	if (fruit->name) {
-		printf("~destruct: %s\n", fruit->name);
-		free(fruit->name);
-	}
-}
-
-int fruit_cmp(const void *self, const void *other)
-{
-	const struct fruit *a = self;
-	const struct fruit *b = other;
-
-	return strcmp(a->name, b->name);
-}
-
-const struct mcc_object_interface fruit_obj_intf = {
-	.size = sizeof(struct fruit),
-	.dtor = &fruit_dtor,
-	.cmp = &fruit_cmp,
-	.hash = NULL,
-};
-
-void print_fruit_list(struct mcc_list *list, const char *fmt, ...)
-{
-	struct mcc_list_iter iter;
-	struct fruit elem;
-	va_list args;
-
-	va_start(args, fmt);
-	vprintf(fmt, args);
-	va_end(args);
-
-	mcc_list_iter_init(list, &iter);
-	while (mcc_iter_next(&iter, &elem))
-		printf("{ .name = %s, .color = %d }\n", elem.name, elem.color);
-}
-
-int test_automatic_destructor_call()
-{
-	printf("%s\n", __FUNCTION__);
-	struct mcc_list *l = mcc_list_new(&fruit_obj_intf);
-
-	mcc_list_push_back(l, &(struct fruit){strdup("Orange"), 0});
-	mcc_list_push_back(l, &(struct fruit){strdup("Watermelon"), 1});
-	mcc_list_push_back(l, &(struct fruit){strdup("Apple"), 2});
-	mcc_list_push_back(l, &(struct fruit){strdup("Pear"), 3});
-	mcc_list_push_front(l, &(struct fruit){strdup("Pineapple"), 4});
-	mcc_list_push_front(l, &(struct fruit){strdup("Banana"), 5});
-	mcc_list_push_front(l, &(struct fruit){strdup("Grape"), 6});
-	mcc_list_push_front(l, &(struct fruit){strdup("Strawberry"), 7});
-	print_fruit_list(l, "Before sorting (by name):\n");
-
-	mcc_list_sort(l);
-	print_fruit_list(l, "After sorting (by name):\n");
-
-	mcc_list_delete(l);
-	puts("testing done");
-	return 0;
-}
-
-bool is_ordered(struct mcc_list *list)
-{
-	struct mcc_list_iter iter;
-	int a, b;
-
-	mcc_list_iter_init(list, &iter);
-	mcc_iter_next(&iter, &a);
-	while (mcc_iter_next(&iter, &b)) {
-		if (b < a)
+	iter = mcc_list_iter_new(d);
+	assert(iter != NULL);
+	for (size_t i = 0; mcc_list_iter_next(iter, (void **)&ref); i++) {
+		if (*ref != a[i])
 			return false;
-		a = b;
 	}
 	return true;
 }
 
-int test_sorting()
+static void test_push_and_pop()
 {
-	printf("%s\n", __FUNCTION__);
-	struct mcc_list *l = mcc_list_new(INT);
+	struct mcc_list *d = mcc_list_new(mcc_int());
+	assert(d != NULL);
+	assert(!mcc_list_push_back(d, &(int){0}));
+	assert(!mcc_list_push_back(d, &(int){1}));
+	assert(!mcc_list_push_back(d, &(int){2}));
+	assert(!mcc_list_push_back(d, &(int){3}));
+	assert(!mcc_list_push_back(d, &(int){4}));
+	assert(!mcc_list_push_front(d, &(int){5}));
+	assert(!mcc_list_push_front(d, &(int){6}));
+	assert(!mcc_list_push_front(d, &(int){7}));
+	assert(equals(d, (int[]){7, 6, 5, 0, 1, 2, 3, 4}));
+	mcc_list_pop_back(d);
+	mcc_list_pop_back(d);
+	assert(equals(d, (int[]){7, 6, 5, 0, 1, 2}));
+	mcc_list_push_back(d, &(int){8});
+	mcc_list_push_front(d, &(int){9});
+	assert(equals(d, (int[]){9, 7, 6, 5, 0, 1, 2, 8}));
+	mcc_list_pop_back(d);
+	mcc_list_pop_back(d);
+	mcc_list_pop_back(d);
+	mcc_list_pop_back(d);
+	mcc_list_pop_back(d);
+	mcc_list_pop_back(d);
+	mcc_list_pop_back(d);
+	assert(equals(d, (int[]){9}));
+	mcc_list_drop(d);
+}
 
-	srand(time(NULL));
-	for (size_t i = 0; i < 51431; i++)
-		mcc_list_push_back(l, &(int){rand()});
-	for (size_t i = 0; i < 78413; i++)
-		mcc_list_push_front(l, &(int){rand()});
+static void test_insert_and_remove()
+{
+	struct mcc_list *d = mcc_list_new(mcc_int());
+	assert(d != NULL);
+	assert(!mcc_list_insert(d, 0, &(int){0}));
+	assert(!mcc_list_insert(d, 0, &(int){1}));
+	assert(!mcc_list_insert(d, 1, &(int){2}));
+	assert(!mcc_list_insert(d, 1, &(int){3}));
+	assert(!mcc_list_insert(d, 2, &(int){4}));
+	assert(!mcc_list_insert(d, 3, &(int){5}));
+	assert(!mcc_list_insert(d, 3, &(int){6}));
+	assert(!mcc_list_insert(d, 5, &(int){7}));
+	assert(equals(d, (int[]){1, 3, 4, 6, 5, 7, 2, 0}));
+	mcc_list_remove(d, 5);
+	assert(equals(d, (int[]){1, 3, 4, 6, 5, 2, 0}));
+	mcc_list_remove(d, 2);
+	assert(equals(d, (int[]){1, 3, 6, 5, 2, 0}));
+	mcc_list_remove(d, 5);
+	assert(equals(d, (int[]){1, 3, 6, 5, 2}));
+	assert(!mcc_list_insert(d, 3, &(int){8}));
+	assert(equals(d, (int[]){1, 3, 6, 8, 5, 2}));
+	assert(!mcc_list_insert(d, mcc_list_len(d), &(int){9}));
+	assert(equals(d, (int[]){1, 3, 6, 8, 5, 2, 9}));
+	assert(!mcc_list_insert(d, mcc_list_len(d), &(int){10}));
+	assert(!mcc_list_insert(d, mcc_list_len(d), &(int){11}));
+	assert(!mcc_list_insert(d, mcc_list_len(d), &(int){12}));
+	assert(equals(d, (int[]){1, 3, 6, 8, 5, 2, 9, 10, 11, 12}));
+	mcc_list_drop(d);
+	d = mcc_list_new(mcc_int());
+	assert(d != NULL);
+	assert(!mcc_list_push_back(d, &(int){0}));
+	assert(!mcc_list_push_back(d, &(int){1}));
+	assert(!mcc_list_push_back(d, &(int){2}));
+	assert(!mcc_list_push_back(d, &(int){3}));
+	assert(!mcc_list_push_front(d, &(int){4}));
+	assert(!mcc_list_push_front(d, &(int){5}));
+	assert(!mcc_list_push_front(d, &(int){6}));
+	assert(!mcc_list_push_front(d, &(int){7}));
+	assert(!mcc_list_insert(d, 3, &(int){8}));
+	assert(equals(d, (int[]){7, 6, 5, 8, 4, 0, 1, 2, 3}));
+	mcc_list_drop(d);
+}
 
-	mcc_list_sort(l);
+static void test_drop_call()
+{
+	struct fruit tmp;
+	struct mcc_list *d = mcc_list_new(&fruit_);
+	assert(d != NULL);
+	assert(!mcc_list_push_back(d, fruit_new(&tmp, "Orange")));
+	assert(!mcc_list_push_back(d, fruit_new(&tmp, "Watermelon")));
+	assert(!mcc_list_push_back(d, fruit_new(&tmp, "Apple")));
+	assert(!mcc_list_push_back(d, fruit_new(&tmp, "Pear")));
+	assert(!mcc_list_push_back(d, fruit_new(&tmp, "Pineapple")));
+	assert(!mcc_list_push_back(d, fruit_new(&tmp, "Banana")));
+	assert(!mcc_list_push_back(d, fruit_new(&tmp, "Grape")));
+	assert(!mcc_list_push_back(d, fruit_new(&tmp, "Strawberry")));
+	putchar('\t');
+	mcc_list_pop_back(d);
+	putchar('\t');
+	mcc_list_pop_back(d);
+	putchar('\t');
+	mcc_list_remove(d, 3);
+	assert(!mcc_list_sort(d));
+	mcc_list_drop(d);
+}
 
-	if (is_ordered(l))
-		puts("OK");
-	else
-		puts("ERR");
-
-	mcc_list_delete(l);
-	puts("testing done");
-	return 0;
+static void test_iterator()
+{
+	struct mcc_list_iter *iter;
+	int *elem;
+	size_t i;
+	int a[] = {15, 14, 13, 12, 11, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	struct mcc_list *d = mcc_list_new(mcc_int());
+	assert(d != NULL);
+	assert(!mcc_list_push_back(d, &(int){0}));
+	assert(!mcc_list_push_back(d, &(int){1}));
+	assert(!mcc_list_push_back(d, &(int){2}));
+	assert(!mcc_list_push_back(d, &(int){3}));
+	assert(!mcc_list_push_back(d, &(int){4}));
+	assert(!mcc_list_push_back(d, &(int){5}));
+	assert(!mcc_list_push_back(d, &(int){6}));
+	assert(!mcc_list_push_back(d, &(int){7}));
+	assert(!mcc_list_push_back(d, &(int){8}));
+	assert(!mcc_list_push_back(d, &(int){9}));
+	assert(!mcc_list_push_front(d, &(int){10}));
+	assert(!mcc_list_push_front(d, &(int){11}));
+	assert(!mcc_list_push_front(d, &(int){12}));
+	assert(!mcc_list_push_front(d, &(int){13}));
+	assert(!mcc_list_push_front(d, &(int){14}));
+	assert(!mcc_list_push_front(d, &(int){15}));
+	iter = mcc_list_iter_new(d);
+	assert(iter != NULL);
+	for (i = 0; mcc_list_iter_next(iter, (void **)&elem); i++)
+		assert(a[i] == *elem);
+	mcc_list_drop(d);
 }
 
 int main(void)
 {
 	test_push_and_pop();
 	test_insert_and_remove();
-	test_automatic_destructor_call();
-	test_sorting();
+	test_drop_call();
+	test_iterator();
+	puts("testing done");
 	return 0;
 }
